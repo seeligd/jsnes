0ea219ff8639f3796885e4a519dee2e271fd9a4d
David Seelig <seeligd@gmail.com>
2012-07-20 00:05:07 -0700

    don't use events for input

--- source/ui.js
+++ source/ui.js
@@ -166,10 +166,6 @@ if (typeof jQuery !== 'undefined') {
                     }).
                     bind('keypress', function(evt) {
                         self.nes.keyboard.keyPress(evt);
-                    }).
-                    bind('controller', function(evt,data) {
-                      _.each(data.press, function(k) { self.nes.keyboard.setTouch(k,"pressed");});
-                      _.each(data.release, function(k) { self.nes.keyboard.setTouch(k)});
                     });
             
                 /*
@@ -181,6 +177,11 @@ if (typeof jQuery !== 'undefined') {
             };
         
             UI.prototype = {    
+            		sendInput: function(data) {
+            			var self = this;
+									_.each(data.press, function(k) { self.nes.keyboard.setTouch(k,"pressed");});
+									_.each(data.release, function(k) { self.nes.keyboard.setTouch(k)});
+            		},
                 loadROM: function() {
                     var self = this;
                     self.updateStatus("Downloading...");
