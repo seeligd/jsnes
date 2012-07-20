ee128cae9970ac0d1da250d91c41c9ada3967040
David Seelig <seeligd@gmail.com>
2012-07-19 17:47:40 -0700

    remove logging

--- source/ui.js
+++ source/ui.js
@@ -168,7 +168,6 @@ if (typeof jQuery !== 'undefined') {
                         self.nes.keyboard.keyPress(evt);
                     }).
                     bind('controller', function(evt,data) {
-                      console.log("ui: controller:", evt,data);
                       _.each(data.press, function(k) { self.nes.keyboard.setTouch(k,"pressed");});
                       _.each(data.release, function(k) { self.nes.keyboard.setTouch(k)});
                     });
